13233692baffcfe532ae64b5e3b102e56dd86e8e
dch1114 <129824716+dch1114@users.noreply.github.com>
2024-02-15 09:59:40 +0900

    [level 1] Title: 음양 더하기, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+        public int solution(int[] absolutes, bool[] signs)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < absolutes.Length; i++)
+                answer += absolutes[i] * (signs[i] ? 1 : -1);
+
+            return answer;
+        }
+}
